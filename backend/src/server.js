fb5bf12606a0fa90232bead218e512afd6b3fcdf
Chriss Chris <zenskech@outlook.com>
2020-11-16 22:13:14 +0500

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const port = 3000;
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/messages", (req, res) => {
   const messages = ["Hello", "Hey", "hi"];
