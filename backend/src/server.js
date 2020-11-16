ca02d13394dd057a2700bd60e20649f96e1c108a
Chriss Chris <zenskech@outlook.com>
2020-11-16 21:12:53 +0500

    edit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 3000;
+
+app.use(cors());
+
+app.get("/messages", (req, res) => {
+  const messages = ["Hello", "Hey", "hi"];
+
+  res.send(messages);
+});
+
+app.listen(port, () => console.log("app is connected"));
