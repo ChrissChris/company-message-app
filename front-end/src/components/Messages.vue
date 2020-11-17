d98971195424e47a2fa1b8a3536b66151cc83006
Chriss Chris <zenskech@outlook.com>
2020-11-17 22:37:16 +0500

    edit

--- front-end/src/components/Messages.vue
+++ front-end/src/components/Messages.vue
@@ -1,7 +1,7 @@
 <template>
   <v-flex sm8 offset-sm2>
     <v-card>
-      <v-toolbar color="blue">
+      <v-toolbar dark>
         <v-toolbar-title>Messages</v-toolbar-title>
       </v-toolbar>
       <v-list>
