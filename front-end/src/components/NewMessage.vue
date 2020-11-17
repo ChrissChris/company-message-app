7eb13b444c5bdc3be5b1adf4428baac8ce91454f
Chriss Chris <zenskech@outlook.com>
2020-11-17 21:54:29 +0500

    Update NewMessage.vue

--- front-end/src/components/NewMessage.vue
+++ front-end/src/components/NewMessage.vue
@@ -1,7 +1,7 @@
 <template>
   <v-flex sm8 offset-sm2>
     <v-card>
-      <v-toolbar dark>
+      <v-toolbar color="blue">
         <v-toolbar-title>New Message</v-toolbar-title>
       </v-toolbar>
       <v-form>
